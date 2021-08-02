22a274b012601a7c6c9503a4eebaa06af1431799
Salam AL QAISSY <salqaissy@asi.fr>
2021-08-02 17:06:04 +0200

    Add project files.

---
+++ Alert/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Alert
+{
+    class Program
+    {
+        /// <summary>
+        /// Salam
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                string pathUrl = args[0].Replace(args[0].Split(':')[0] + ':', "");
+                try
+                {
+                    Process.Start(@pathUrl);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
+    }
+}
